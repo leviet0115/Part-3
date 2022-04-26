77521c2501628fea811c696172993817a3fbfda0
Le Viet <le.viet@MacBook-Pro-2.local>
2022-04-26 14:08:43 +0300

    exercise 3.4

--- phonebook-server/index.js
+++ phonebook-server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const persons = [
+var persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -44,6 +44,12 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== Number(id));
+  res.status(204).end();
 });
 
 const PORT = 3001;
